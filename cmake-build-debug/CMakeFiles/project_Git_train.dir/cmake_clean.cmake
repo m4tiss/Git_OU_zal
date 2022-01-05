4c76e85fd91cc00e990f64b385cd52ef0962acaa
m4tiss <mateusz039@op.pl>
2022-01-05 14:10:01 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/project_Git_train.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project_Git_train.dir/main.cpp.obj"
+  "libproject_Git_train.dll.a"
+  "project_Git_train.exe"
+  "project_Git_train.exe.manifest"
+  "project_Git_train.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/project_Git_train.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
